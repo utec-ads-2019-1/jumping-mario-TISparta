74c6e11a463cdf6a8cd94dcec9daa599842aaf7a
TISparta <jose.garcia@utec.edu.pe>
2019-03-28 20:58:38 -0500

    Solucion

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
+int main () {
+  int tc;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++) {
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    int high = 0, low = 0;
+    for (int i = 1; i < n; i++) {
+      if (arr[i] > arr[i - 1]) high++;
+      if (arr[i] < arr[i - 1]) low++;
+    }
+    cout << "Case " << t << ": " << high << ' ' << low << '\n';
+  }
+  return (0);
 }
-
